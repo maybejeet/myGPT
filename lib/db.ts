6fddbfca9ab67fc761575ab838341d6165bd0b44
maybejeet <contactjeet247@gmail.com>
2026-08-22 20:11:08 +0530

    Added messages actions

--- lib/db.ts
+++ lib/db.ts
@@ -31,26 +31,3 @@ export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 if(process.env.NODE_ENV !== "production"){
     globalForPrisma.prisma = prisma;
 }
-
-// import { PrismaPg } from "@prisma/adapter-pg";
-// import { PrismaClient } from "./generated/prisma/client";
-
-// // Function to create a new PrismaClient instance
-// export function createPrismaClient() {
-//   const adapter = new PrismaPg({
-//     connectionString: process.env.DATABASE_URL,
-//   });
-//   return new PrismaClient({ adapter });
-// }
-
-// // Next.js compatibility: avoid hot-reload issues and edge serverless issues by using a global singleton
-// const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient | undefined};
-
-// export const prisma =
-//   globalForPrisma.prisma ||
-//   createPrismaClient();
-
-// // Attach to global object in development to prevent multiple instances during hot-reload
-// if (process.env.NODE_ENV !== "production") {
-//   globalForPrisma.prisma = prisma;
-// }
